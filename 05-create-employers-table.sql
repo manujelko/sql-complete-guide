89816019cbe3314863509e6a0910ddbcc31f1885
Manuel Šarić <manujelko@gmail.com>
2022-08-17 20:41:36 +0200

    Default column values

--- 05-create-employers-table.sql
+++ 05-create-employers-table.sql
@@ -3,5 +3,5 @@ CREATE TABLE employers (
     company_address VARCHAR(300),
     -- revenue FLOAT(5, 2), -- Approximation (total number of digits, maximum number of digits after the dot)
     yearly_revenue NUMERIC(5, 2),
-    is_hiring BOOLEAN
+    is_hiring BOOLEAN DEFAULT FALSE
 );
