c44937be23dd513a98d5e50212c66bee38f89fff
Manuel Šarić <manujelko@gmail.com>
2022-08-17 20:24:11 +0200

    Create employers table

---
+++ 05-create-employers-table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE employers (
+    company_name VARCHAR(250),
+    company_address VARCHAR(300),
+    -- revenue FLOAT(5, 2), -- Approximation (total number of digits, maximum number of digits after the dot)
+    yearly_revenue NUMERIC(5, 2),
+    is_hiring BOOLEAN
+);
