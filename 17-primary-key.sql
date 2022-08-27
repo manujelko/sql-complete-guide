d16b0d0c9225bb7d3cb46ba06a045a8dbfb0468c
Manuel Šarić <manujelko@gmail.com>
2022-08-27 23:07:28 +0100

    Auto-incrementing IDs and primary keys

---
+++ 17-primary-key.sql
@@ -0,0 +1,27 @@
+-- DROP TABLE users;
+-- DROP TABLE employers;
+-- DROP TABLE conversations;
+
+CREATE TYPE emplyment_status AS ENUM('self-employed', 'employed', 'unemployed');
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(300) NOT NULL,
+    yearly_salary INT CHECK (yearly_salary > 0),
+    current_status employment_status
+);
+
+CREATE TABLE employers (
+    id SERIAL PRIMARY KEY,
+    company_name VARCHAR(300) NOT NULL,
+    company_address VARCHAR(300) NOT NULL,
+    yearly_revenue float CHECK (yearly_revenue > 0),
+    is_hiring BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE conversations (
+    id SERIAL PRIMARY KEY,
+    user_id INT,
+    employer_id INT,
+    message TEXT NOT NULL
+);
