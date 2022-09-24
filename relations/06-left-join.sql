c476aebf545106ca952950898826804641119c50
Manuel Šarić <manujelko@gmail.com>
2022-09-24 21:42:59 +0200

    Combining multiple LEFT JOINs

--- relations/06-left-join.sql
+++ relations/06-left-join.sql
@@ -1,3 +1,8 @@
+-- SELECT *
+-- FROM addresses AS a
+-- LEFT JOIN users AS u ON a.id = u.address_id;
+
 SELECT *
 FROM addresses AS a
-LEFT JOIN users AS u ON a.id = u.address_id;
+LEFT JOIN users AS u ON a.id = u.address_id
+LEFT JOIN cities AS c ON c.id = a.city_id;
