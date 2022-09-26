0794dc1c051aef693b7c34f16e0c9d649cf9340f
Manuel Šarić <manujelko@gmail.com>
2022-09-26 14:56:21 +0200

    More string functions

--- gym/02-strings-functions.sql
+++ gym/02-strings-functions.sql
@@ -5,5 +5,14 @@
 -- SELECT first_name || ' ' || last_name
 -- FROM memberships;
 
-SELECT CONCAT('$ ', price)
-FROM memberships;
+-- SELECT CONCAT('$ ', price)
+-- FROM memberships;
+
+-- SELECT LOWER('ABC');
+
+-- SELECT LENGTH('ABC');
+
+-- LEADING
+-- TRAILING
+-- BOTH
+SELECT TRIM(TRAILING ' ' FROM 'male   ');
