064fb5ecfb8bdea328d32d45471f71d355a80d1d
Manuel Šarić <manujelko@gmail.com>
2022-09-26 15:06:48 +0200

    Working with weekdays

--- gym/03-date-time-functions.sql
+++ gym/03-date-time-functions.sql
@@ -1,2 +1,8 @@
-SELECT EXTRACT(MONTH FROM last_checkin), last_checkin
+-- SELECT EXTRACT(MONTH FROM last_checkin), last_checkin
+-- FROM memberships;
+
+-- SELECT EXTRACT(ISODOW FROM last_checkin), last_checkin
+-- FROM memberships;
+
+SELECT last_checkin::TIMESTAMP::DATE, last_checkin::TIMESTAMP::TIME
 FROM memberships;
