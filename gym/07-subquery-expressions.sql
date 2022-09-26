ae021beed3a0cc342fb9e49fa4636dc22a2fc2aa
Manuel Šarić <manujelko@gmail.com>
2022-09-26 15:34:48 +0200

    Working with subquery expressions and EXISTS

--- gym/07-subquery-expressions.sql
+++ gym/07-subquery-expressions.sql
@@ -2,8 +2,16 @@
 -- FROM customers
 -- WHERE email = 'max@test.com';
 
-SELECT EXISTS(
-	SELECT first_name, last_name
-	FROM customers
-	WHERE email = 'manu@test.com'
+-- SELECT EXISTS(
+-- 	SELECT first_name, last_name
+-- 	FROM customers
+-- 	WHERE email = 'manu@test.com'
+-- );
+
+SELECT id
+FROM orders AS o
+WHERE EXISTS(
+	SELECT email
+	FROM customers AS c
+	WHERE o.customer_id = c.id AND c.email = 'manu@test.com'
 );
