a7ccdba4b3bf8fc03f2ba5d543873368c8348e00
Manuel Šarić <manujelko@gmail.com>
2022-09-27 16:34:31 +0200

    GROUP BY

--- my_events/10-more-queries.sql
+++ my_events/10-more-queries.sql
@@ -1,4 +1,8 @@
 -- SELECT * FROM users
 -- WHERE first_name LIKE 'Ma%';
 
-SELECT COUNT(*) FROM locations;
+-- SELECT COUNT(*) FROM locations;
+
+SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
+LEFT JOIN locations AS loc ON loc.city_name = c.name
+GROUP BY c.name;
