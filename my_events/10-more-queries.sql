1d70939bf89567e281a3360d52150a7c9e47d4fe
Manuel Šarić <manujelko@gmail.com>
2022-09-27 16:35:33 +0200

    HAVING

--- my_events/10-more-queries.sql
+++ my_events/10-more-queries.sql
@@ -3,6 +3,11 @@
 
 -- SELECT COUNT(*) FROM locations;
 
+-- SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
+-- LEFT JOIN locations AS loc ON loc.city_name = c.name
+-- GROUP BY c.name;
+
 SELECT c.name, COUNT(loc.id) AS num_locations FROM cities AS c
 LEFT JOIN locations AS loc ON loc.city_name = c.name
-GROUP BY c.name;
+GROUP BY c.name
+HAVING COUNT(loc.id) > 1;
