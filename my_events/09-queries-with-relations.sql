4f621391f15015dd85430159bef7a12ab7f33970
Manuel Šarić <manujelko@gmail.com>
2022-09-27 16:24:08 +0200

    LEFT JOIN

--- my_events/09-queries-with-relations.sql
+++ my_events/09-queries-with-relations.sql
@@ -1,17 +1,24 @@
 -- SELECT e.id, e.name, e.date_planned FROM events AS e
 -- INNER JOIN locations AS l ON e.location_id = l.id;
 
-SELECT
-	e.id AS event_id,
-	e.name,
-	e.date_planned,
-	loc.title,
-	loc.street,
-	loc.house_number,
-	loc.city_name,
-	u.first_name,
-	u.last_name
-FROM events AS e
-INNER JOIN locations AS loc ON e.location_id = loc.id
-INNER JOIN events_users AS eu ON eu.event_id = e.id
-INNER JOIN users AS u on eu.user_id = u.id;
+-- SELECT
+-- 	e.id AS event_id,
+-- 	e.name,
+-- 	e.date_planned,
+-- 	loc.title,
+-- 	loc.street,
+-- 	loc.house_number,
+-- 	loc.city_name,
+-- 	u.first_name,
+-- 	u.last_name
+-- FROM events AS e
+-- INNER JOIN locations AS loc ON e.location_id = loc.id
+-- INNER JOIN events_users AS eu ON eu.event_id = e.id
+-- INNER JOIN users AS u on eu.user_id = u.id;
+
+-- SELECT * FROM locations AS loc
+-- LEFT JOIN events AS e ON e.location_id = loc.id;
+
+SELECT * FROM cities AS c
+LEFT JOIN locations AS loc ON loc.city_name = c.name
+LEFT JOIN events AS e ON e.location_id = loc.id;
